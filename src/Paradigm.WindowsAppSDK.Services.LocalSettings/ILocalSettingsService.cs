3b6cff1fdfca4c4d1aae47947c78ddafb67879da
Iván Falletti <ivan@miracledevs.com>
2022-11-03 12:08:27 -0300

    Added LocalSettings service

---
+++ src/Paradigm.WindowsAppSDK.Services.LocalSettings/ILocalSettingsService.cs
@@ -0,0 +1,25 @@
+﻿using Paradigm.WindowsAppSDK.Services.Interfaces;
+
+namespace Paradigm.WindowsAppSDK.Services.LocalSettings
+{
+    public interface ILocalSettingsService : IService
+    {
+        /// <summary>
+        /// Gets the stored settings.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T GetStoredSettings<T>();
+
+        /// <summary>
+        /// Stores the settings.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        void StoreSettings<T>(T settings);
+
+        /// <summary>
+        /// Resets to default settings.
+        /// </summary>
+        void ResetToDefaultSettings();
+    }
+}
---
+++ src/Paradigm.WindowsAppSDK.Services.LocalSettings/LocalSettingsService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Windows.Storage;
+
+namespace Paradigm.WindowsAppSDK.Services.LocalSettings
+{
+    public class LocalSettingsService : ILocalSettingsService
+    {
+        private const string SettingsKey = "local-settings";
+
+        /// <summary>
+        /// Gets the stored settings.
+        /// </summary>
+        /// <returns></returns>
+        public T GetStoredSettings<T>()
+        {
+            var storedSettings = ApplicationData.Current.LocalSettings.Values[SettingsKey]?.ToString();
+            return !string.IsNullOrWhiteSpace(storedSettings) ? JsonSerializer.Deserialize<T>(storedSettings) : default;
+        }
+
+        /// <summary>
+        /// Stores the settings.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        public void StoreSettings<T>(T settings)
+        {
+            if (settings == null)
+                return;
+
+            ApplicationData.Current.LocalSettings.Values[SettingsKey] = JsonSerializer.Serialize(settings);
+        }
+
+        /// <summary>
+        /// Resets to default settings.
+        /// </summary>
+        public void ResetToDefaultSettings()
+        {
+            ApplicationData.Current.LocalSettings.Values.Clear();
+        }
+    }
+}
