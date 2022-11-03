dec984c38bd03f25a0405eaf6cb70e3b6c75d785
Iván Falletti <ivan@miracledevs.com>
2022-11-03 14:10:13 -0300

    Added MainThreadDispatcher service. Changed ViewModels project framework

---
+++ src/Paradigm.WindowsAppSDK.Services.MainThreadDispatcher/IMainThreadDispatcherService.cs
@@ -0,0 +1,24 @@
+﻿using Paradigm.WindowsAppSDK.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Paradigm.WindowsAppSDK.Services.MainThreadDispatcher
+{
+    /// <summary>
+    /// Provides a way of executing methods in the main thread.
+    /// </summary>
+    public interface IMainThreadDispatcherService : IService
+    {
+        /// <summary>
+        /// Runs the function on the main thread.
+        /// </summary>
+        /// <param name="function">The function.</param>
+        Task RunAsync(Func<Task> function);
+
+        /// <summary>
+        /// Runs the specified action.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        void Run(Action action);
+    }
+}
