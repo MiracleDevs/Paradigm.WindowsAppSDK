f1afc794dad6bd9fbb1ad2adba9fbbf0eab9a045
Iván Falletti <ivan@miracledevs.com>
2022-11-04 15:37:15 -0300

    Created Services unit tests project

---
+++ src/Paradigm.WindowsAppSDK.Services.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Paradigm.WindowsAppSDK.Services.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
