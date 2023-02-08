ce2def2ff781cbe017d441596b0dbe3d4d96dd56
Iván Falletti <ivan@miracledevs.com>
2023-02-08 15:03:05 -0300

    Added TaskExtensions and EmptyStringConverter

---
+++ src/Paradigm.WindowsAppSDK.Xaml/Converters/EmptyStringConverter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Paradigm.WindowsAppSDK.Xaml.Converters
+{
+    public class EmptyStringConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="language">The language.</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var stringValue = value as string;
+            return string.IsNullOrEmpty(stringValue) ? parameter?.ToString() ?? "-" : stringValue;
+        }
+
+        /// <summary>
+        /// Converts the back.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="language">The language.</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
